d172cd993435af5940627c17b248420127c19713
Dvir Guttman <dvir.guttman@verizonmedia.com>
2022-08-18 13:29:25 +0300

    script update 2
    Signed-off-by: Dvir Guttman <dvir.guttman@verizonmedia.com>

--- goupdate.sh
+++ goupdate.sh
@@ -1,15 +1,14 @@
 #!/bin/bash -ex
 
 sudo yum -y install wget
-go version
-which go
+sudo yum erase -y go
+go version || true
+which go || true
 LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text)";
 LATEST_GO_DOWNLOAD_URL="https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
 wget ${LATEST_GO_DOWNLOAD_URL}
-rm -rf /usr/local/go 
 tar -C /usr/local -xzf go*.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
-ls -l /etc
-sudo bash -c "echo 'export PATH=\$PATH:...' >> /etc/bashrc"
-sudo bash -c "echo 'export PATH=\$PATH:...' >> /etc/shrc"
+sudo bash -c "echo 'export PATH=\$PATH:/usr/local/go/bin' >> /etc/bashrc"
+sudo bash -c "echo 'export PATH=\$PATH:/usr/local/go/bin' >> /etc/shrc"
 go version
