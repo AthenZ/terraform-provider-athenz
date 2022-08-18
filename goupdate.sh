34462b57ae0258cdd02bd371a75ef3a69ef764ce
Dvir Guttman <dvir.guttman@verizonmedia.com>
2022-08-18 13:48:17 +0300

    soft link all bin
    Signed-off-by: Dvir Guttman <dvir.guttman@verizonmedia.com>

--- goupdate.sh
+++ goupdate.sh
@@ -8,6 +8,6 @@ LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text)";
 LATEST_GO_DOWNLOAD_URL="https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
 wget ${LATEST_GO_DOWNLOAD_URL}
 tar -C /usr/local -xzf go*.linux-amd64.tar.gz
-sudo ln -s /usr/local/go/bin/go /usr/bin
+sudo ln -s /usr/local/go/bin/* /usr/bin
 
 go version
