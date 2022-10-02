d420422fa94d93335f40fd75350fb37aad27d182
Dvir Guttman <dvir.guttman@verizonmedia.com>
2022-10-02 15:08:09 +0300

    organize execution scripts
    Signed-off-by: Dvir Guttman <dvir.guttman@verizonmedia.com>

--- execution/local-sys-test.sh
+++ execution/local-sys-test.sh
@@ -23,7 +23,7 @@ make install_local
 
 # get latest provider version 
 VERSION="$(ls -tr ~/.terraform.d/plugins/yahoo/provider/athenz | tail -1)"
-sed -i -e "s/version = \"x.x.x\"/version = \"$VERSION\"/g" "local-sys-test/sys-test_provider.tf"
+sed -i -e "s/version = \"x.x.x\"/version = \"$VERSION\"/g" "sys-test/sys-test_provider.tf"
 
 EXIT_CODE=0
 
@@ -32,7 +32,7 @@ export SYS_TEST_CERT="$(pwd)/docker/sample/domain-admin/domain_admin_cert.pem"
 export SYS_TEST_KEY="$(pwd)/docker/sample/domain-admin/domain_admin_key.pem"
 
 # First, create the sys test domain and run several tests using the latest terraform provider
-cd local-sys-test
+cd sys-test
 if ! terraform init ; then
     echo "terraform init failed!"
     EXIT_CODE=1
@@ -55,21 +55,21 @@ zms-cli \
   -c ${SYS_TEST_CA_CERT} \
   -key ${SYS_TEST_KEY} \
   -cert ${SYS_TEST_CERT} \
-  show-domain terraform-provider | sed 's/modified: .*/modified: XXX/' > local-sys-test/terraform-sys-test-results
+  show-domain terraform-provider | sed 's/modified: .*/modified: XXX/' > sys-test/terraform-sys-test-results
 
 echo 'Terraform results: '
-cat local-sys-test/terraform-sys-test-results
+cat sys-test/terraform-sys-test-results
 echo 'Expected results: '
-cat local-sys-test/expected-terraform-sys-test-results
+cat sys-test/expected-terraform-sys-test-results
 
 # make sure the expected domain is same as zms-cli result
-if ! diff -w local-sys-test/terraform-sys-test-results local-sys-test/expected-terraform-sys-test-results ; then
+if ! diff -w sys-test/terraform-sys-test-results sys-test/expected-terraform-sys-test-results ; then
     echo "expected domain is NOT same!"
     EXIT_CODE=1
 fi
 
 # destroy resources
-cd local-sys-test
+cd sys-test
 terraform apply --destroy -auto-approve -var="cacert=$SYS_TEST_CA_CERT" -var="cert=$SYS_TEST_CERT" -var="key=$SYS_TEST_KEY" -var-file="variables/sys-test-policies-versions-vars.tfvars" -var-file="variables/sys-test-groups-vars.tfvars" -var-file="variables/prod.tfvars" -var-file="variables/sys-test-services-vars.tfvars" -var-file="variables/sys-test-roles-vars.tfvars" -var-file="variables/sys-test-policies-vars.tfvars"
 sed -i -e "s/version = \"$VERSION\"/version = \"x.x.x\"/g" "sys-test_provider.tf"
 rm -fr .terraform* *-e terraform* 
