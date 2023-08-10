add0e9191100835852ab49d26d82492720ef7be1
Dvir Guttman <dvir.guttman@verizonmedia.com>
2023-08-10 20:28:01 +0300

    debug message for git tag
    Signed-off-by: Dvir Guttman <dvir.guttman@verizonmedia.com>

--- scripts/git-tag.sh
+++ scripts/git-tag.sh
@@ -76,6 +76,7 @@ echo "Prev version is: $(cat PREVIOUS_VERSION)"
 
 echo "Getting git tag version"
 $GIT_VERSION --prefix "$PREFIX" bump $RELEASE_TYPE | tee VERSION
+echo "version after bumping is: $VERSION"
 /opt/sd/meta set git.version `cat VERSION`
 echo "New version is: $(cat VERSION)"
 
