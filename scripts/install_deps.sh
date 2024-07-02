b197f00f801550d667d42608637fb67182b02e28
Henry Avetisyan <hga@yahooinc.com>
2024-07-02 13:24:33 -0700

    move to ubuntu image
    Signed-off-by: Henry Avetisyan <hga@yahooinc.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -28,7 +28,7 @@ apt-get update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable"
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
 docker system info
